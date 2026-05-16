b8ab0e48c8b0996fd374f39156e2011d615baa15
Dryzs <marwin.bendel@gmail.com>
2026-05-16 19:36:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="gymjournal-v17";
-const FILES=["./", "./index.html", "./style.css?v=17", "./app.js?v=17", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+const CACHE="gymjournal-v18";
+const FILES=["./", "./index.html", "./style.css?v=18", "./app.js?v=18", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))})
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()})
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
